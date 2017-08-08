8d4ebdc1dade3807314212c25ba67853f15bf3bd
Nick Stair <nickstair@Nicks-MacBook-Pro-2.local>
2017-08-08 14:56:43 -0400

    Added a vector

--- git_prog1.cc
+++ git_prog1.cc
@@ -4,7 +4,16 @@
 using namespace std;
 
 int main(){
-  cout << "hello there world" << endl;
+  cout << "hello there world\n" << endl;
+
+  std::vector<int> v(10);
+
+  v[0] = 1
+
+  for(int i=0; i < 9; i++){
+    v[i+1] = 2*v[i]
+    cout << "V[i] = " << v[i] << endl;
+  }
 
   return 0;
 }
